76afacba73327d8865c53904ac1d7dc815034885
caposcar1998 <oscarcontrerascap@gmail.com>
2021-05-04 13:34:54 -0500

    Se puede crear usaurio

---
+++ app/src/main/java/edu/itesm/appchallenge/LoginFragment.kt
@@ -0,0 +1,69 @@
+package edu.itesm.appchallenge
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_login.*
+
+
+
+class LoginFragment : Fragment() {
+
+
+    private lateinit var auth: FirebaseAuth
+    lateinit var correoMandar : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // Inicializa objetos:
+        auth = Firebase.auth
+        iniciarSesionBoton.setOnClickListener {iniciarSesion()}
+
+        registroBoton.setOnClickListener { view?.findNavController()?.navigate(R.id.toRegistroFragment) }
+
+
+    }
+
+    fun iniciarSesion(){
+        if(correoInicio.text.isNotEmpty() && contraInicio.text.isNotEmpty() ){
+            correoMandar = correoInicio.text.toString()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                correoInicio.text.toString(),
+                contraInicio.text.toString()
+            ).addOnCompleteListener{
+                if(it.isSuccessful){
+                    Toast.makeText(this.context,"Bienvenido", Toast.LENGTH_LONG).show()
+
+                    val action = LoginFragmentDirections.loginToFirst()
+                    view?.findNavController()?.navigate(action)
+
+                }else{
+                    Toast.makeText(this.context,"Usuario o contraseña son incorrectos", Toast.LENGTH_LONG).show()
+                }
+            }
+        }else{
+            Toast.makeText(this.context,"No dejes campos en blanco", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/edu/itesm/appchallenge/RegistroFragment.kt
@@ -0,0 +1,92 @@
+package edu.itesm.appchallenge
+
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import edu.itesm.appchallenge.R
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.fragment_registro.*
+
+@Parcelize
+data class Usuario(var nombre: String, var correo:String) : Parcelable
+
+class RegistroFragment : Fragment() {
+
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dataBase : FirebaseFirestore
+    private lateinit var nuevoUsuario: Usuario
+    lateinit var correoMandar : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // Inicializa objetos:
+        nuevoUsuario = Usuario("","")
+        auth = Firebase.auth
+        dataBase = Firebase.firestore
+        crearCuenta.setOnClickListener { crearUsuario() }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    private fun usuarioCreado(){
+
+        val builder = AlertDialog.Builder(this.requireContext())
+        with(builder){
+            Toast.makeText(this.context,"Usuario creado con éxito", Toast.LENGTH_LONG).show()
+            val action = RegistroFragmentDirections.registroToMain2()
+            view?.findNavController()?.navigate(action)
+        }
+    }
+
+    fun crearUsuario(){
+        if (contrasena.text.toString() == corroborarContrasena.text.toString()){
+
+            if (correo.text.isNotEmpty() && contrasena.text.isNotEmpty() ){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    correo.text.toString(),
+                    contrasena.text.toString()
+                ).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        correo.text.clear()
+                        contrasena.text.clear()
+                        usuarioCreado()
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(this.context,it.toString(), Toast.LENGTH_LONG).show()
+                }
+            }else{Toast.makeText(this.context,"No dejes campos vacios", Toast.LENGTH_LONG).show()}
+        }else{Toast.makeText(this.context,"Contraseñas no coinciden", Toast.LENGTH_LONG).show() }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registro, container, false)
+    }
+
+
+}
